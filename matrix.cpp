2e61a5af137cac791b26fd06da7effe85599d394
mathikneipp <mkneipp@udesa.edu.ar>
2025-03-13 13:50:15 -0300

    cambio por vector en ej 1

--- matrix.cpp
+++ matrix.cpp
@@ -1,23 +1,18 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 
-void destroy_matrix(int** matrix, int r) {
-    for (int i = 0; i < r; i++) {
-        free(matrix[i]);
-    }
-    free(matrix);
-}
-
-void fill_matrix(int** matrix, int row, int col, int n) {
+void fill_matrix(vector<vector<int>>& matrix, int row, int col, const int n) {
     matrix[row][col] = (row * n) + col + 1;
 }
 
-void print_inverted_pos(int** matrix, int row, int col, int n) {
+void print_inverted_pos(vector<vector<int>>& matrix, int row, int col, const int n) {
     int inverted_row = (n-1) - row;
     int inverted_col = (n-1) - col;
     std::cout << 'M' << n << '[' <<  inverted_row << "][" << inverted_col << "] = " << matrix[inverted_row][inverted_col] << std::endl;
 }
 
-void traverse_matrix(int** matrix, int n, void (*work)(int**, int, int, int)) {
+void traverse_matrix(vector<vector<int>>& matrix, const int n, void (*work)(vector<vector<int>>&, int, int, int)) {
     int row, col; 
     for (int i = 0; i < n*n; i++) {
         row = (int) (i / n);
@@ -26,34 +21,20 @@ void traverse_matrix(int** matrix, int n, void (*work)(int**, int, int, int)) {
     }
 }
 
-void print_matrix(int** matrix, int n) {
+void print_matrix(vector<vector<int>>& matrix, const int n) {
     traverse_matrix(matrix, n, print_inverted_pos);
 }
 
-int** create_matrix(int n) {
-    int** matrix = (int**) malloc((size_t) n * sizeof(int*));
-    if (matrix == NULL) {
-        throw "No se pudo crear la matriz";
-        return NULL;
-    }
-    for (int i = 0; i < n; i++) {
-        matrix[i] = (int*) malloc((size_t) n * sizeof(int));
-        if (matrix[i] == NULL) {
-            throw "No se pudo crear la matriz";
-            destroy_matrix(matrix, i);
-            return NULL;
-        }
-    }
+vector<vector<int>> create_matrix(int n) {
+    vector<vector<int>> matrix(n, vector<int>(n));
+
     traverse_matrix(matrix, n, fill_matrix);
     return matrix;
 }
 
 int main() {
-    int n;
-    std::cout << "Ingrese el tamaño de la matriz: ";
-    std::cin >> n;
-    int** matrix = create_matrix(n);
+    const int n = 3;
+    vector<vector<int>> matrix = create_matrix(n);
     print_matrix(matrix, n);
-    destroy_matrix(matrix, n); // Libera la memoria de la matriz
     return 0;
 }
